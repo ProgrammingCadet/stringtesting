88da778d22c337ac5c55299bb1875667d2f928d9
ProgrammingCadet <C26Dustin.Mock@afacademy.af.edu>
2023-10-24 08:22:54 -0600

    attempted to get max strlen

--- strtesting.c
+++ strtesting.c
@@ -5,10 +5,12 @@
 
 int main(void) {
     char str1[100] = ""; // Initialize using a static character array
+    strcpy(str1, "This is a 100 character long sentence, and I'm not gonna stop typing until I get to 100 chars. I ne");
     printf("----- str1 -----\nstrlen: %ld\nsizeof: %ld\n", strlen(str1), sizeof(str1)); // Print strlen and sizeof str1
     printf("Address: %lx\n", (long)str1);
 
     char* str2 = (char*)malloc(100 * sizeof(char)); // Initialize using malloc and char*
+    strcpy(str2, "This is a 100 character long sentence, and I'm not gonna stop typing until I get to 100 chars. I ne");
     printf("----- str2 -----\nstrlen: %ld\nsizeof: %ld\n", strlen(str2), sizeof(str2)); // Print strlen and sizeof str2
     printf("Address: %lx\n", (long)str2);
 
